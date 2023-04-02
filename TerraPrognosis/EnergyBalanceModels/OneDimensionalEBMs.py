6aeb381f800a944fa81696fed457e065194bc674
Raagulbharatwaj <74106637+Raagulbharatwaj@users.noreply.github.com>
2023-04-02 22:00:56 +0530

    Added One dimensional EBM model still need to debug it

--- TerraPrognosis/EnergyBalanceModels/OneDimensionalEBMs.py
+++ TerraPrognosis/EnergyBalanceModels/OneDimensionalEBMs.py
@@ -1,86 +1,96 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class OneDimEBM:
-    def __init__(self, n_bands=10, n_years=100):
-        self.sigma = 5.67e-8    # Stefan-Boltzmann constant
-        self.solar_constant = 1366    # Solar constant
-        self.specific_heat = 4.0e7    # Specific heat of the atmosphere
-        self.diffusivity = 0.5556    # Thermal diffusivity of the atmosphere
-        self.timestep = 60*60*24    # Time step in seconds
-        self.n_bands = n_bands    # Number of latitudinal bands
-        self.n_years = n_years    # Number of years to simulate
-        
-        # Initialize state variables
-        self.temperature = np.zeros((n_years+1, n_bands))
-        self.albedo = np.zeros((n_years+1, n_bands))
-        self.emissivity = np.zeros((n_years+1, n_bands))
-        self.insolation = np.zeros((n_years+1, n_bands))
-        self.time = np.arange(n_years+1)
-        
-        # Set initial conditions
-        self.temperature[0,:] = 288    # Surface temperature in K
-        self.albedo[0,:] = 0.3    # Albedo
-        self.emissivity[0,:] = 0.9    # Emissivity
-        
-    def solar_declination_angle(self, day):
-        return np.radians(23.45) * np.sin(np.radians(360/365.25*(day-81)))
+class Simple1DimEBM:
+    def __init__(self,layers,years,albedos):
+        self.layers   = layers
+        self.years    = years
+        self.bands    = np.linspace(-90, 90, layers)
+        self.temps    = np.zeros((layers,years*365+1))
+        self.temp     = np.zeros(layers)
+        self.albedos  = albedos
     
-    def hour_angle(self, time):
-        return np.radians(360/24*(time-12))
+    def local_solar_time(self,day):
+        band_width = 180/self.layers
+        n = day
+        B = (n-1)*360/365
+        E = 229.18 * (0.000075 + 0.001868 * np.cos(np.radians(B)) - 0.032077 * np.sin(np.radians(B)) - 0.014615 * np.cos(np.radians(2 * B)) - 0.040849 * np.sin(np.radians(2 * B)))
+        local_solar_times = []
+        for i in range(self.layers):
+            standard_meridian = -180 + (i + 0.5) * band_width
+            local_solar_time = 12 + (4 * (0 - standard_meridian) - E) / 60
+            local_solar_times.append(local_solar_time)
+        return local_solar_times
     
-    def calculate_insolation(self, day, time, lat):
-        declination = self.solar_declination_angle(day)
-        angle_of_incidence = np.arcsin(np.sin(np.radians(lat))*np.sin(declination) + np.cos(np.radians(lat))*np.cos(declination)*np.cos(self.hour_angle(time)))
-        return self.solar_constant * np.sin(angle_of_incidence)
+    def solar_declination(self,day):
+        angle  = np.deg2rad(360/365 * (day - 81))
+        decliniations = []
+        for band in self.bands:
+            declination = np.arcsin(np.sin(np.deg2rad(band))*np.sin(angle))
+            decliniations.append(np.rad2deg(declination))
+        return decliniations
+
+    def hour_angle(self,local_solar_times):
+        hour_angles = []
+        for local_solar_time in local_solar_times:
+            hour_angle = 15 * (local_solar_time - 12)
+            hour_angles.append(hour_angle)
+        return hour_angles
     
-    def step_forward(self, day, lat):
-        # Calculate insolation
-        time = np.arange(0, 24, 24/self.n_bands)
-        for i in range(self.n_bands):
-            self.insolation[day,i] = self.calculate_insolation(day, time[i], lat[i])
-            
-        # Calculate net energy flux
-        flux_in = self.insolation[day,:] * (1 - self.albedo[day,:])
-        flux_out = self.emissivity[day,:] * self.sigma * self.temperature[day,:]**4
-        net_flux = flux_in - flux_out
-        
-        # Update temperature
-        delta_temp = self.timestep / (self.specific_heat * self.diffusivity) * (net_flux[1:] - net_flux[:-1])
-        self.temperature[day+1,1:-1] = self.temperature[day,1:-1] + delta_temp
-        
-        # Update albedo and emissivity
-        self.albedo[day+1,:] = np.where(self.temperature[day,:] < 273.15, 0.7, 0.3)
-        self.emissivity[day+1,:] = np.where(self.temperature[day,:] < 273.15, 0.95, 0.9)
-        
-        # Set boundary conditions
-        self.temperature[day+1,0] = self.temperature[day,0] + self.timestep * (self.insolation[day,0] * (1 - self.albedo[day,0]) - self.emissivity[day,0] * self.sigma * self.temperature[day,0]**4) / (self.specific_heat * self.diffusivity)
-        self.temperature[day+1,-1] = self.temperature[day,-1] + self.timestep * (self.insolation[day,-1] * (1 - self.albedo[day,-1]) - self.emissivity[day,-1] * self.sigma * self.temperature[day,-1]**4) / (self.specific_heat * self.diffusivity)
+    def solar_zenith_angle(self,day):
+        local_solar_times = self.local_solar_time(day)
+        decliniations     = self.solar_declination(day)
+        hour_angles       = self.hour_angle(local_solar_times)
+        zenith_angles     = []
+        for i in range(self.layers):
+            zenith_angle = np.arccos(np.sin(np.deg2rad(decliniations[i]))*np.sin(np.deg2rad(self.bands[i])) + np.cos(np.deg2rad(decliniations[i]))*np.cos(np.deg2rad(self.bands[i]))*np.cos(np.deg2rad(hour_angles[i])))
+            zenith_angles.append(np.rad2deg(zenith_angle))
+        return zenith_angles
+    
+    def solar_insolation(self,day):
+        zenith_angles = self.solar_zenith_angle(day)
+        solar_insolation = []
+        for zenith_angle in zenith_angles:
+            solar_insolation.append(1367.0 * np.cos(np.deg2rad(zenith_angle)))
+        return solar_insolation
+    
+    def outgoing_longwave_radiation(self,year,day):
+        index = (year-1)*365 + day
+        outgoing_longwave_radiations = []
+        sigma = 5.67e-8
+        emissivity = 0.61
+        for i in range(self.layers):
+            outgoing_longwave_radiation = sigma * emissivity * np.power(self.temps[i,index-1],4)
+            outgoing_longwave_radiations.append(outgoing_longwave_radiation)
+        return outgoing_longwave_radiations
+
+    def incoming_solar_radiation(self,year,day):
+        incoming_solar_radiations = []
+        solar_insolations = self.solar_insolation(day)
+        for i in range(self.layers):
+            incoming_solar_radiation = solar_insolations[i] * (1 - self.albedos[i])
+            incoming_solar_radiations.append(incoming_solar_radiation)
+        return incoming_solar_radiations
     
     def run_simulation(self):
-        for i in range(self.n_years):
-            for j in range(self.n_bands):
-                self.step_forward(i, j)
-
+        for year in range(1, self.years+1):
+            for day in range(1, 366):
+                incoming_radiation = self.incoming_solar_radiation(year, day)
+                for i in range(self.layers):
+                    self.temp[i] = np.float64((incoming_radiation[i]*(0.61)*(5.67e-8))**(1/4))
+                self.temps[:,(year-1)*365+day] = self.temp
+        
     def plot_results(self):
-        fig, ax = plt.subplots(1, 3, figsize=(12, 4))
-        ax[0].plot(self.time, self.temperature[:,0], label='Pole')
-        ax[0].plot(self.time, self.temperature[:,-1], label='Equator')
-        ax[0].set_xlabel('Time (years)')
-        ax[0].set_ylabel('Temperature (K)')
-        ax[0].legend()
-        
-        ax[1].plot(self.time, self.albedo[:,0], label='Pole')
-        ax[1].plot(self.time, self.albedo[:,-1], label='Equator')
-        ax[1].set_xlabel('Time (years)')
-        ax[1].set_ylabel('Albedo')
-        ax[1].legend()
-        
-        ax[2].plot(self.time, self.emissivity[:,0], label='Pole')
-        ax[2].plot(self.time, self.emissivity[:,-1], label='Equator')
-        ax[2].set_xlabel('Time (years)')
-        ax[2].set_ylabel('Emissivity')
-        ax[2].legend()
-        
+        # ploth the temprature for each layer over time on the same plot
+        plt.figure(figsize=(10, 5))
+        for i in range(self.layers):
+            plt.plot(self.temps[i], label='Layer {}'.format(i+1))
+        plt.xlabel('Day')
+        plt.ylabel('Temperature (K)')
+        plt.legend()
         plt.show()
+
+
+
+    
 
--- TerraPrognosis/main.py
+++ TerraPrognosis/main.py
@@ -1,39 +1,13 @@
 import numpy as np
-from EnergyBalanceModels.ZeroDimensionalEBMs import LayeredZeroDimEBM
+from EnergyBalanceModels.OneDimensionalEBMs import Simple1DimEBM
 
-#troposphere
-troposphere_albedos = np.random.uniform(0.06, 0.12, 100)
-troposphere_emissivities = np.random.uniform(0.85, 0.95, 100)
-
-# Stratosphere
-stratosphere_albedos = np.random.uniform(0.25, 0.35, 100)
-stratosphere_emissivities = np.random.uniform(0.85, 0.95, 100)
-
-# Mesosphere
-mesosphere_albedos = np.random.uniform(0.4, 0.5, 100)
-mesosphere_emissivities = np.random.uniform(0.85, 0.95, 100)
-
-# Thermosphere
-thermosphere_albedos = np.random.uniform(0.6, 0.7, 100)
-thermosphere_emissivities = np.random.uniform(0.85, 0.95,100)
-
-albedos = np.stack((troposphere_albedos, stratosphere_albedos, mesosphere_albedos, thermosphere_albedos), axis=0)
-emissivities = np.stack((troposphere_emissivities, stratosphere_emissivities, mesosphere_emissivities, thermosphere_emissivities), axis=0)
-
-# Initialize model
-curr_temp = 288.0  # K
-num_layers = 4
-layer_thickness = 1.0  # m
-
-# Create the model object
-model = LayeredZeroDimEBM(curr_temp, num_layers, layer_thickness)
-
-temperature, delta_t = model.run_model(albedos, emissivities)
-
-# Plot the results
-model.plot_results(temperature, delta_t)
-
-
+layers = 5
+years = 1
+initial_temp = [273.15] * layers
+albedos = [0.3, 0.5, 0.6, 0.5, 0.3]
+model = Simple1DimEBM(layers, years,albedos)
+model.run_simulation()
+model.plot_results()
 
 
 
