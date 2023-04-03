d6c37f91025631d958904aa7da79e6fe97ce2754
Raagulbharatwaj <74106637+Raagulbharatwaj@users.noreply.github.com>
2023-04-03 21:16:08 +0530

    simple OneDimensionalEBM is working properly

--- TerraPrognosis/EnergyBalanceModels/OneDimensionalEBMs.py
+++ TerraPrognosis/EnergyBalanceModels/OneDimensionalEBMs.py
@@ -2,38 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Simple1DimEBM:
-    def __init__(self,layers,years,albedos):
-        self.layers   = layers
-        self.years    = years
-        self.bands    = np.linspace(-90, 90, layers)
-        self.temps    = np.zeros((layers,years*365+1))
-        self.temp     = np.zeros(layers)
-        self.albedos  = albedos
+    def __init__(self,layers,years,albedos,time_zones):
+        self.layers    = layers
+        self.years     = years
+        self.bands     = np.linspace(0, 180, layers)
+        self.temps     = np.zeros((layers,np.ceil(years*365).astype(np.int64)))
+        self.temp      = np.zeros(layers)
+        self.time_zone = time_zones
+        self.albedos   = albedos
     
     def local_solar_time(self,day):
         band_width = 180/self.layers
-        n = day
-        B = (n-1)*360/365
-        E = 229.18 * (0.000075 + 0.001868 * np.cos(np.radians(B)) - 0.032077 * np.sin(np.radians(B)) - 0.014615 * np.cos(np.radians(2 * B)) - 0.040849 * np.sin(np.radians(2 * B)))
+        B = (day-81)*360/365
+        E = 9.87*np.sin(np.deg2rad(2*B)) - 7.53*np.cos(np.deg2rad(B)) - 1.5*np.sin(np.deg2rad(B))
         local_solar_times = []
         for i in range(self.layers):
-            standard_meridian = -180 + (i + 0.5) * band_width
-            local_solar_time = 12 + (4 * (0 - standard_meridian) - E) / 60
+            standard_meridian = 0 + (i + 0.5) * band_width
+            LSTM = 15*self.time_zone[i]
+            time_correction   = 4*(standard_meridian - LSTM) + E
+            local_solar_time  = 12 + (time_correction/60)
             local_solar_times.append(local_solar_time)
         return local_solar_times
     
     def solar_declination(self,day):
-        angle  = np.deg2rad(360/365 * (day - 81))
-        decliniations = []
-        for band in self.bands:
-            declination = np.arcsin(np.sin(np.deg2rad(band))*np.sin(angle))
-            decliniations.append(np.rad2deg(declination))
-        return decliniations
-
+        angle  = np.deg2rad((360/365) * (day - 81))
+        declination = 23.45 * np.sin(angle)
+        declinations = [declination for i in range(self.layers)]
+        return declinations
+        
     def hour_angle(self,local_solar_times):
         hour_angles = []
         for local_solar_time in local_solar_times:
-            hour_angle = 15 * (local_solar_time - 12)
+            hour_angle = 15 * (local_solar_time - 6)
             hour_angles.append(hour_angle)
         return hour_angles
     
@@ -43,7 +43,11 @@ class Simple1DimEBM:
         hour_angles       = self.hour_angle(local_solar_times)
         zenith_angles     = []
         for i in range(self.layers):
-            zenith_angle = np.arccos(np.sin(np.deg2rad(decliniations[i]))*np.sin(np.deg2rad(self.bands[i])) + np.cos(np.deg2rad(decliniations[i]))*np.cos(np.deg2rad(self.bands[i]))*np.cos(np.deg2rad(hour_angles[i])))
+            if i == len(self.bands)-1:
+                lat = self.bands[i] + 90 /2
+            else:
+                lat = self.bands[i]+self.bands[i+1]/2
+            zenith_angle = np.arccos(np.sin(np.deg2rad(decliniations[i])) * np.sin(np.deg2rad(lat)) + np.cos(np.deg2rad(decliniations[i])) * np.cos(np.deg2rad(lat)) * np.cos(np.deg2rad(hour_angles[i])))
             zenith_angles.append(np.rad2deg(zenith_angle))
         return zenith_angles
     
@@ -51,20 +55,11 @@ class Simple1DimEBM:
         zenith_angles = self.solar_zenith_angle(day)
         solar_insolation = []
         for zenith_angle in zenith_angles:
-            solar_insolation.append(1367.0 * np.cos(np.deg2rad(zenith_angle)))
+            solar_insolation.append(1367.0 / 4 * np.sin(np.deg2rad(zenith_angle)))
         return solar_insolation
     
-    def outgoing_longwave_radiation(self,year,day):
-        index = (year-1)*365 + day
-        outgoing_longwave_radiations = []
-        sigma = 5.67e-8
-        emissivity = 0.61
-        for i in range(self.layers):
-            outgoing_longwave_radiation = sigma * emissivity * np.power(self.temps[i,index-1],4)
-            outgoing_longwave_radiations.append(outgoing_longwave_radiation)
-        return outgoing_longwave_radiations
 
-    def incoming_solar_radiation(self,year,day):
+    def incoming_solar_radiation(self,day):
         incoming_solar_radiations = []
         solar_insolations = self.solar_insolation(day)
         for i in range(self.layers):
@@ -75,10 +70,10 @@ class Simple1DimEBM:
     def run_simulation(self):
         for year in range(1, self.years+1):
             for day in range(1, 366):
-                incoming_radiation = self.incoming_solar_radiation(year, day)
+                incoming_radiation = self.incoming_solar_radiation(day)
                 for i in range(self.layers):
-                    self.temp[i] = np.float64((incoming_radiation[i]*(0.61)*(5.67e-8))**(1/4))
-                self.temps[:,(year-1)*365+day] = self.temp
+                    self.temp[i] = ((incoming_radiation[i])/(0.61*5.67e-8))**(1/4)
+                self.temps[:,(year-1)*365+day-1] = self.temp
         
     def plot_results(self):
         # ploth the temprature for each layer over time on the same plot
--- TerraPrognosis/main.py
+++ TerraPrognosis/main.py
@@ -4,15 +4,23 @@ from EnergyBalanceModels.OneDimensionalEBMs import Simple1DimEBM
 layers = 5
 years = 1
 initial_temp = [273.15] * layers
-albedos = [0.3, 0.5, 0.6, 0.5, 0.3]
-model = Simple1DimEBM(layers, years,albedos)
+albedos = [0.4, 0.6, 0.7,0.8,0.86]
+time_zones = [0, 3, 6, 9, 12]
+model = Simple1DimEBM(layers, years,albedos,time_zones)
+
+
 model.run_simulation()
 model.plot_results()
 
 
 
-
-
-
-
-
+'''
+    def outgoing_longwave_radiation(self,year,day):
+        index = (year-1)*365 + day
+        outgoing_longwave_radiations = []
+        sigma = 5.67e-8
+        emissivity = 0.61
+        for i in range(self.layers):
+            outgoing_longwave_radiation = sigma * emissivity * np.power(self.temps[i,index-1],4)
+            outgoing_longwave_radiations.append(outgoing_longwave_radiation)
+        return outgoing_longwave_radiations'''
